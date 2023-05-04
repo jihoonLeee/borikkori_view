237aeabc4d4cf8700425e7526d54ab2050ca4be0
lee jihoon <jihoon2723@naver.com>
2023-05-04 11:24:34 +0900

    안쓰는 컴포넌트 제거

--- src/Components/Questions/Question.jsx
+++ src/Components/Questions/Question.jsx
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
