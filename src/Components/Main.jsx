b4e07b713586ed1f8120c18680053bb255fdb258
lee jihoon <jihoon2723@naver.com>
2023-05-07 22:12:11 +0900

    오타 수정

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,12 +4,12 @@ export default function Main({onClick}) {
     const [start, setStart] = useState(false);
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center pb-24">
-            <span className="py-2">우리 강아지 MBTI 는?</span>
+            <span className="py-2">우리 강아지가 사람이라면?</span>
             <button className='rounded-lg bg-green-200 w-56 h-28' 
                 onClick={() => { 
                     setStart(!start);
                     onClick();
-                }} >테스트 하러 가기~</button>
+                }} >MBTI 테스트 하러 가기~</button>
         </div>
     );
 }
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -41,7 +41,7 @@ export default function Result(props){
       return (
         visible ? <Loading></Loading> :
         <div>
-            <img id="result" className='m-auto block pb-10' alt="main_dog" src={`/images/results/${resultName}_result.png`} />
+            <img id="result" className='m-auto block pb-10' alt="result" src={`/images/results/${resultName}_result.png`} />
             <button onClick={onCapture}> <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" ><path d="M11.5 8h1v7.826l2.5-3.076.753.665-3.753 4.585-3.737-4.559.737-.677 2.5 3.064v-7.828zm7 12h-13c-2.481 0-4.5-2.019-4.5-4.5 0-2.178 1.555-4.038 3.698-4.424l.779-.14.043-.79c.185-3.447 3.031-6.146 6.48-6.146 3.449 0 6.295 2.699 6.479 6.146l.043.79.78.14c2.142.386 3.698 2.246 3.698 4.424 0 2.481-2.019 4.5-4.5 4.5m.979-9.908c-.212-3.951-3.473-7.092-7.479-7.092s-7.267 3.141-7.479 7.092c-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408"/></svg> </button>
               <span className='pr-10 pl-2 font-bold'>결과 저장하기</span>
             <button onClick={onCopyLink}>
